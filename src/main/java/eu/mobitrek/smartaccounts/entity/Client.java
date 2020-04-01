a642ff152d639ad56f054d1e89ec1d25fc3fd894
tups <tarmo.tubro@gmail.com>
2020-04-02 00:34:04 +0300

    client

---
+++ src/main/java/eu/mobitrek/smartaccounts/entity/Client.java
@@ -0,0 +1,18 @@
+package eu.mobitrek.smartaccounts.entity;
+
+public class Client {
+    public String id;
+    public String name;
+    public String regCode;
+    public String vatNumber;
+    public boolean ignoreOnVatDeclaration;
+    public int invoiceDueDate;
+    public String vatPc;
+    public String pdfTemplateInvoice;
+    public String accountUnpaid;
+    public String accountPrepayment;
+    public String dateCreated;
+    public String dateUpdated;
+    public String createdBy;
+    public String updatedBy;
+}
